3ae8c03ffdd99c044694916c3c4cfd2c0b9fddd5
GianDutra <chikegang@gmail.com>
2023-08-03 18:00:39 -0300

    Adicionando rotas vercel 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom'
 import { Router } from "./Routes";
 
 function App() {
@@ -9,9 +9,9 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <BrowserRouter>
+      <HashRouter>
         <Router />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider> 
   )
 }
