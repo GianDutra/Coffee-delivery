7fecfb3d4195f0dee37a5fa5b8fc87959e857521
GianDutra <chikegang@gmail.com>
2023-08-03 16:01:50 -0300

    Testando responsividade 4.0

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -10,7 +10,7 @@ export const CheckoutPageContainer = styled.form`
     @media screen and (max-width: 768px) {
       flex-direction: column;
       padding-left: 2rem;
-      
+      overflow-x: hidden;
     }
 `
 
