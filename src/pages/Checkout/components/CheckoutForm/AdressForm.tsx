a45ab490d7f3177dffd8547ecba496fbeac7cf3b
GianDutra <chikegang@gmail.com>
2023-08-04 15:35:00 -0300

    Adicionando Endereço automatico com o CEP

--- src/pages/Checkout/components/CheckoutForm/AdressForm.tsx
+++ src/pages/Checkout/components/CheckoutForm/AdressForm.tsx
@@ -1,19 +1,41 @@
+import React from "react";
+import axios from "axios";
 import { useFormContext } from "react-hook-form";
 import { Input } from "../../../../components/Input";
 import { AddressFormContainer } from "./styles";
 
-interface ErrorsType{
+interface ErrorsType {
   errors: {
     [key: string]: {
-      message: string
-    }
-  }
+      message: string;
+    };
+  };
 }
 
 export function AddressForm() {
-  const { register, formState } = useFormContext()
+  const { register, formState, setValue } = useFormContext();
 
-  const { errors } = formState as unknown as ErrorsType
+  const { errors } = formState as unknown as ErrorsType;
+
+  const handleCepBlur = async (event: React.FocusEvent<HTMLInputElement>) => {
+    const cep = event.target.value.replace(/\D/g, ""); // Remove caracteres não numéricos do CEP
+
+    if (cep.length !== 8) return; // Verifica se o CEP tem 8 dígitos
+
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+      if (response.data) {
+        // Atualiza os campos com os dados do endereço
+        setValue("street", response.data.logradouro);
+        setValue("district", response.data.bairro);
+        setValue("city", response.data.localidade);
+        setValue("uf", response.data.uf);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar CEP:", error);
+    }
+  };
 
   return (
     <AddressFormContainer>
@@ -22,6 +44,7 @@ export function AddressForm() {
         type="number"
         className="cep"
         {...register("cep")}
+        onBlur={handleCepBlur} // Adiciona o evento onBlur para buscar o CEP
         error={errors.cep?.message}
       />
       <Input
@@ -45,23 +68,23 @@ export function AddressForm() {
         rightText="Opcional"
       />
       <Input 
-      placeholder="Bairro" 
-      className="neighborhood"
-      {...register("district")}
-      error={errors.district?.message} 
+        placeholder="Bairro" 
+        className="neighborhood"
+        {...register("district")}
+        error={errors.district?.message} 
       />
       <Input 
-      placeholder="Cidade" 
-      className="city" 
-      {...register("city")}
-      error={errors.city?.message}
+        placeholder="Cidade" 
+        className="city" 
+        {...register("city")}
+        error={errors.city?.message}
       />
-      <Input placeholder="UF"
-       className="uf"
-       {...register("uf")} 
-       error={errors.uf?.message}
+      <Input 
+        placeholder="UF"
+        className="uf"
+        {...register("uf")} 
+        error={errors.uf?.message}
       />
-     
     </AddressFormContainer>
   );
 }
