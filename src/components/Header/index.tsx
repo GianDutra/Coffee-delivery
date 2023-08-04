5c05775ba85805ea95e07384ec319c201a258c2c
GianDutra <chikegang@gmail.com>
2023-08-04 16:04:52 -0300

    Corrigindo erro do deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from "./styles";
 import coffeeLogo from "../../assets/coffee-delivery-logo.svg";
 import { MapPin, ShoppingCart } from "phosphor-react";
