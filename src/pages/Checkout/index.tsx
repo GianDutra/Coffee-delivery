5d685f49915798fa5cd3dc9ccd2d7f4e97f5cc0b
GianDutra <chikegang@gmail.com>
2023-08-04 19:21:48 -0300

    Carrinho vazio? Voltar ao catalogo

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,61 +1,76 @@
-import { CheckoutForm } from "./components/CheckoutForm";
-import { SelectedCoffees } from "./components/SelectedCoffees";
-import { CheckoutPageContainer } from "./styles";
-import * as zod from 'zod'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm, FormProvider } from 'react-hook-form'
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../../hooks/useCart";
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from 'zod';
+import { SelectedCoffees } from "./components/SelectedCoffees";
+import { CheckoutForm } from "./components/CheckoutForm";
+import { CheckoutPageContainer, EmptyCheckout, SectionBaseStyle } from "./styles";
+import { ShoppingCart } from "phosphor-react";
+import { Button } from "./components/Button";
 
 enum PaymentMethods {
-    credit = "credit",
-    debit = "debit",
-    money = "money",
-  }
-  
-  const confirmOrderFormValidationSchema = zod.object({
-    cep: zod.string().min(1, "Informe o CEP"),
-    street: zod.string().min(1, "Informe o Rua"),
-    number: zod.string().min(1, "Informe o Número"),
-    complement: zod.string(),
-    district: zod.string().min(1, "Informe o Bairro"),
-    city: zod.string().min(1, "Informe a Cidade"),
-    uf: zod.string().min(1, "Informe a UF"),
-    paymentMethod: zod.nativeEnum(PaymentMethods, {
-      errorMap: () => {
-        return { message: "Informe o método de pagamento" };
-      },
-    }),
+  credit = "credit",
+  debit = "debit",
+  money = "money",
+}
+
+// Definir o esquema de validação para o formulário
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, "Informe o CEP"),
+  street: zod.string().min(1, "Informe o Rua"),
+  number: zod.string().min(1, "Informe o Número"),
+  complement: zod.string(),
+  district: zod.string().min(1, "Informe o Bairro"),
+  city: zod.string().min(1, "Informe a Cidade"),
+  uf: zod.string().min(1, "Informe a UF"),
+  paymentMethod: zod.nativeEnum(PaymentMethods, {
+    errorMap: () => {
+      return { message: "Informe o método de pagamento" };
+    },
+  }),
+});
+
+// Obter o tipo dos dados do formulário a partir do esquema de validação
+export type ConfirmOrderFormData = zod.infer<typeof confirmOrderFormValidationSchema>;
+
+export function CheckoutPage() {
+  const { cartQuantity } = useCart();
+  const navigate = useNavigate();
+  const { cleanCart } = useCart();
+
+  const checkoutForm = useForm<ConfirmOrderFormData>({
+    resolver: zodResolver(confirmOrderFormValidationSchema)
   });
-  
-  export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>;
-  
-  type ConfirmOrderFormData = OrderData;
 
-    export function CheckoutPage() {
-        const checkoutForm = useForm<ConfirmOrderFormData>({
-            resolver: zodResolver(confirmOrderFormValidationSchema)
-        })
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    navigate("/success", {
+      state: data,
+    });
+    cleanCart();
+  }
 
-
-    const { handleSubmit } = checkoutForm
-
-    const navigate = useNavigate();
-    const { cleanCart } = useCart();
-  
-    function handleConfirmOrder(data: ConfirmOrderFormData) {
-      navigate("/success", {
-        state: data,
-      });
-      cleanCart();
-    }
-    
+  if (cartQuantity === 0) {
     return (
-        <FormProvider {...checkoutForm}>
-            <CheckoutPageContainer className="container" onSubmit={handleSubmit(handleConfirmOrder)}>
-            <CheckoutForm /> 
-            <SelectedCoffees />
-            </CheckoutPageContainer>
-        </FormProvider>
-    )
+      <CheckoutPageContainer>
+      <SectionBaseStyle className="container">
+      <EmptyCheckout variant="purple">
+      <ShoppingCart size={48} />
+      <p>Seu carrinho está vazio.</p>
+      <p>Gostaria de explorar nossa seleção de cafés premium?</p>
+      <Button text="Ir para o catálogo" onClick={() => navigate("/")} />
+    </EmptyCheckout>
+    </SectionBaseStyle>
+    </CheckoutPageContainer>
+    );
+  }
+
+  return (
+    <FormProvider {...checkoutForm}>
+      <CheckoutPageContainer className="container" onSubmit={checkoutForm.handleSubmit(handleConfirmOrder)}>
+        <CheckoutForm />
+        <SelectedCoffees />
+      </CheckoutPageContainer>
+    </FormProvider>
+  );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import { styled, css } from "styled-components";
 
 export const CheckoutPageContainer = styled.form`
     width: 100%;
@@ -20,4 +20,49 @@ export const SectionBaseStyle = styled.div`
   border-radius: 6px;
   padding: 2.5rem;
 
+`;
+
+interface EmptyCheckoutProps {
+  variant: "purple" | "yellow";
+}
+
+export const EmptyCheckout = styled.div<EmptyCheckoutProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  justify-content: center;
+  display: 100%;
+
+  ${({ variant, theme }) =>
+    variant === "purple" &&
+    css`
+        svg {
+            color: ${theme.colors["brand-purple"]};
+        }
+      
+    `}
+
+  & :first-child {
+    color: ${(props) => props.theme["gray-600"]};
+  }
+
+  
+  a {
+    margin-top: 2rem;
+    text-decoration: none;
+    display: block;
+    padding: 0.75rem 0.5rem;
+    border-radius: 6px;
+    background-color: ${(props) => props.theme["yellow-500"]};
+    font-size: 0.875rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: ${(props) => props.theme.white};
+    cursor: pointer;
+
+    &:hover {
+      background-color: ${(props) => props.theme["yellow-700"]};
+    }
+  }
 `;
